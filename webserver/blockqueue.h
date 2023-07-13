7ac5f747741913e344b72bebd38afcdd6c8cd528
Yang Li <95227304+matey-cloud@users.noreply.github.com>
2023-07-13 21:05:56 +0800

    log日志代码，无法运行版

--- webserver/blockqueue.h
+++ webserver/blockqueue.h
@@ -1,180 +1,164 @@
-//#ifndef BLOCKQUEUE_H
-//#define BLOCKQUEUE_H
+﻿#ifndef BLOCKQUEUE_H
+#define BLOCKQUEUE_H
 
-//#include <mutex>
-//#include <deque>
-//#include <assert.h>
-//#include <condition_variable>
-//#include <sys/time.h>
+#include <mutex>
+#include <deque>
+#include <assert.h>
+#include <condition_variable>
+#include <sys/time.h>
 
-//template<class T>
-//class BlockDeque {
-//public:
-//    explicit BlockDeque(size_t MaxCapacity = 1000);
+template<class T>
+class BlockDeque {
+public:
+    explicit BlockDeque(size_t MaxCapacity = 1000);
 
-//    ~BlockDeque();
+    ~BlockDeque();
 
-//    void clear();
+    void clear();
+    bool empty();
+    bool full();
+    void Close();
+    size_t size();
+    size_t capacity();
+    T front();
+    T back();
 
-//    bool empty();
+    void push_back(const T &item);
+    void push_front(const T &item);
+    bool pop(T &item);
+    bool pop(T &item, int timeout);
+    void flush();
 
-//    bool full();
-
-//    void Close();
-
-//    size_t size();
-
-//    size_t capacity();
-
-//    T front();
-
-//    T back();
-
-//    void push_back(const T &item);
-
-//    void push_front(const T &item);
-
-//    bool pop(T &item);
-
-//    bool pop(T &item, int timeout);
-
-//    void flush();
-
-//private:
-//    std::deque<T> deq_;
-
-//    size_t capacity_;
-
-//    std::mutex mtx_;
-
-//    bool isClose_;
-
-//    std::condition_variable condConsumer_;
-
-//    std::condition_variable condProducer_;
-//};
+private:
+    std::deque<T> m_deq;
+    size_t m_capacity;
+    std::mutex m_mtx;
+    bool m_isClose;
+    std::condition_variable m_condConsumer;
+    std::condition_variable m_condProducer;
+};
 
 
-//template<class T>
-//BlockDeque<T>::BlockDeque(size_t MaxCapacity) :capacity_(MaxCapacity) {
-//    assert(MaxCapacity > 0);
-//    isClose_ = false;
-//}
+template<class T>
+BlockDeque<T>::BlockDeque(size_t MaxCapacity) :capacity_(MaxCapacity) {
+    assert(MaxCapacity > 0);
+    m_isClose = false;
+}
 
-//template<class T>
-//BlockDeque<T>::~BlockDeque() {
-//    Close();
-//}
+template<class T>
+BlockDeque<T>::~BlockDeque() {
+    Close();
+}
 
-//template<class T>
-//void BlockDeque<T>::Close() {
-//    {
-//        std::lock_guard<std::mutex> locker(mtx_);
-//        deq_.clear();
-//        isClose_ = true;
-//    }
-//    condProducer_.notify_all();
-//    condConsumer_.notify_all();
-//};
+template<class T>
+void BlockDeque<T>::Close() {
+    {
+        std::lock_guard<std::mutex> locker(m_mtx);
+        deq_.clear();
+        m_isClose = true;
+    }
+    m_condProducer.notify_all();
+    m_condConsumer.notify_all();
+}
 
-//template<class T>
-//void BlockDeque<T>::flush() {
-//    condConsumer_.notify_one();
-//};
+template<class T>
+void BlockDeque<T>::flush() {
+    m_condConsumer.notify_one();
+}
 
-//template<class T>
-//void BlockDeque<T>::clear() {
-//    std::lock_guard<std::mutex> locker(mtx_);
-//    deq_.clear();
-//}
+template<class T>
+void BlockDeque<T>::clear() {
+    std::lock_guard<std::mutex> locker(mtx_);
+    m_deq.clear();
+}
 
-//template<class T>
-//T BlockDeque<T>::front() {
-//    std::lock_guard<std::mutex> locker(mtx_);
-//    return deq_.front();
-//}
+template<class T>
+T BlockDeque<T>::front() {
+    std::lock_guard<std::mutex> locker(mtx_);
+    return m_deq.front();
+}
 
-//template<class T>
-//T BlockDeque<T>::back() {
-//    std::lock_guard<std::mutex> locker(mtx_);
-//    return deq_.back();
-//}
+template<class T>
+T BlockDeque<T>::back() {
+    std::lock_guard<std::mutex> locker(mtx_);
+    return m_deq.back();
+}
 
-//template<class T>
-//size_t BlockDeque<T>::size() {
-//    std::lock_guard<std::mutex> locker(mtx_);
-//    return deq_.size();
-//}
+template<class T>
+size_t BlockDeque<T>::size() {
+    std::lock_guard<std::mutex> locker(mtx_);
+    return m_deq.size();
+}
 
-//template<class T>
-//size_t BlockDeque<T>::capacity() {
-//    std::lock_guard<std::mutex> locker(mtx_);
-//    return capacity_;
-//}
+template<class T>
+size_t BlockDeque<T>::capacity() {
+    std::lock_guard<std::mutex> locker(mtx_);
+    return m_capacity;
+}
 
-//template<class T>
-//void BlockDeque<T>::push_back(const T &item) {
-//    std::unique_lock<std::mutex> locker(mtx_);
-//    while(deq_.size() >= capacity_) {
-//        condProducer_.wait(locker);
-//    }
-//    deq_.push_back(item);
-//    condConsumer_.notify_one();
-//}
+template<class T>
+void BlockDeque<T>::push_back(const T &item) {
+    std::unique_lock<std::mutex> locker(mtx_);
+    while(m_deq.size() >= m_capacity) {
+        m_condProducer.wait(locker);
+    }
+    m_deq.push_back(item);
+    m_condConsumer.notify_one();
+}
 
-//template<class T>
-//void BlockDeque<T>::push_front(const T &item) {
-//    std::unique_lock<std::mutex> locker(mtx_);
-//    while(deq_.size() >= capacity_) {
-//        condProducer_.wait(locker);
-//    }
-//    deq_.push_front(item);
-//    condConsumer_.notify_one();
-//}
+template<class T>
+void BlockDeque<T>::push_front(const T &item) {
+    std::unique_lock<std::mutex> locker(m_mtx);
+    while(m_deq.size() >= m_capacity) {
+        m_condProducer.wait(locker);
+    }
+    m_deq.push_front(item);
+    m_condConsumer.notify_one();
+}
 
-//template<class T>
-//bool BlockDeque<T>::empty() {
-//    std::lock_guard<std::mutex> locker(mtx_);
-//    return deq_.empty();
-//}
+template<class T>
+bool BlockDeque<T>::empty() {
+    std::lock_guard<std::mutex> locker(m_mtx);
+    return m_deq.empty();
+}
 
-//template<class T>
-//bool BlockDeque<T>::full(){
-//    std::lock_guard<std::mutex> locker(mtx_);
-//    return deq_.size() >= capacity_;
-//}
+template<class T>
+bool BlockDeque<T>::full(){
+    std::lock_guard<std::mutex> locker(m_mtx);
+    return m_deq.size() >= m_capacity;
+}
 
-//template<class T>
-//bool BlockDeque<T>::pop(T &item) {
-//    std::unique_lock<std::mutex> locker(mtx_);
-//    while(deq_.empty()){
-//        condConsumer_.wait(locker);
-//        if(isClose_){
-//            return false;
-//        }
-//    }
-//    item = deq_.front();
-//    deq_.pop_front();
-//    condProducer_.notify_one();
-//    return true;
-//}
+template<class T>
+bool BlockDeque<T>::pop(T &item) {
+    std::unique_lock<std::mutex> locker(m_mtx);
+    while(m_deq.empty()){
+        condConsumer_.wait(locker);
+        if(m_isClose){
+            return false;
+        }
+    }
+    item = m_deq.front();
+    m_deq.pop_front();
+    m_condProducer.notify_one();
+    return true;
+}
 
-//template<class T>
-//bool BlockDeque<T>::pop(T &item, int timeout) {
-//    std::unique_lock<std::mutex> locker(mtx_);
-//    while(deq_.empty()){
-//        if(condConsumer_.wait_for(locker, std::chrono::seconds(timeout))
-//            == std::cv_status::timeout){
-//            return false;
-//        }
-//        if(isClose_){
-//            return false;
-//        }
-//    }
-//    item = deq_.front();
-//    deq_.pop_front();
-//    condProducer_.notify_one();
-//    return true;
-//}
+template<class T>
+bool BlockDeque<T>::pop(T &item, int timeout) {
+    std::unique_lock<std::mutex> locker(m_mtx);
+    while(m_deq.empty()){
+        if(m_condConsumer.wait_for(locker, std::chrono::seconds(timeout))
+            == std::cv_status::timeout){
+            return false;
+        }
+        if(m_isClose){
+            return false;
+        }
+    }
+    item = m_deq.front();
+    m_deq.pop_front();
+    m_condProducer.notify_one();
+    return true;
+}
 
-//#endif // BLOCKQUEUE_H
+#endif // BLOCKQUEUE_H
--- webserver/log.cpp
+++ webserver/log.cpp
@@ -1,189 +1,193 @@
-//#include "log.h"
+﻿#include "log.h"
 
-//using namespace std;
+using namespace std;
 
-//Log::Log() {
-//    lineCount_ = 0;
-//    isAsync_ = false;
-//    writeThread_ = nullptr;
-//    deque_ = nullptr;
-//    toDay_ = 0;
-//    fp_ = nullptr;
-//}
+Log::Log() {
+    m_lineCount = 0;
+    m_toDay = 0;
+    m_isAsync = false;
+    m_fp = nullptr;
+    m_deque = nullptr;
+    m_writeThread = nullptr;
+}
 
-//Log::~Log() {
-//    if(writeThread_ && writeThread_->joinable()) {
-//        while(!deque_->empty()) {
-//            deque_->flush();
-//        };
-//        deque_->Close();
-//        writeThread_->join();
-//    }
-//    if(fp_) {
-//        lock_guard<mutex> locker(mtx_);
-//        flush();
-//        fclose(fp_);
-//    }
-//}
+Log::~Log() {
+    if(m_writeThread && m_writeThread->joinable()) {
+        while(!m_deque->empty()) {
+            m_deque->flush();
+        };
+        m_deque->Close();
+        m_writeThread->join();
+    }
+    if(m_fp) {
+        lock_guard<mutex> locker(m_mtx);
+        flush();
+        fclose(m_fp);
+    }
+}
+// 获取日志级别
+int Log::GetLevel() {
+    lock_guard<mutex> locker(m_mtx);
+    return m_level;
+}
 
-//int Log::GetLevel() {
-//    lock_guard<mutex> locker(mtx_);
-//    return level_;
-//}
+//设置日志级别
+void Log::SetLevel(int level) {
+    lock_guard<mutex> locker(m_mtx);
+    m_level = level;
+}
+//初始化日志记录器。根据参数设置日志级别、路径、后缀、最大队列容量。如果最大队列容量大于0，则启用异步写入模式，在新线程中刷新队列中的日志内容。
+void Log::init(int level = 1, const char* path, const char* suffix,
+               int maxQueueSize) {
+    m_isOpen = true;
+    m_level = level;
+    if(maxQueueSize > 0) {
+        m_isAsync = true;
+        if(!m_deque) {
+            unique_ptr<BlockDeque<std::string>> newDeque(new BlockDeque<std::string>);
+            m_deque = move(newDeque);
 
-//void Log::SetLevel(int level) {
-//    lock_guard<mutex> locker(mtx_);
-//    level_ = level;
-//}
+            std::unique_ptr<std::thread> NewThread(new thread(FlushLogThread));
+            m_writeThread = move(NewThread);
+        }
+    } else {
+        m_isAsync = false;
+    }
 
-//void Log::init(int level = 1, const char* path, const char* suffix,
-//               int maxQueueSize) {
-//    isOpen_ = true;
-//    level_ = level;
-//    if(maxQueueSize > 0) {
-//        isAsync_ = true;
-//        if(!deque_) {
-//            unique_ptr<BlockDeque<std::string>> newDeque(new BlockDeque<std::string>);
-//            deque_ = move(newDeque);
+    m_lineCount = 0;
 
-//            std::unique_ptr<std::thread> NewThread(new thread(FlushLogThread));
-//            writeThread_ = move(NewThread);
-//        }
-//    } else {
-//        isAsync_ = false;
-//    }
+    time_t timer = time(nullptr);
+    struct tm *sysTime = localtime(&timer);
+    struct tm t = *sysTime;
 
-//    lineCount_ = 0;
+    m_path = path;
+    m_suffix = suffix;
+    char fileName[LOG_NAME_LEN] = {0};
+    snprintf(fileName, LOG_NAME_LEN - 1, "%s/%04d_%02d_%02d%s",
+             m_path, t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, m_suffix);
+    m_toDay = t.tm_mday;
 
-//    time_t timer = time(nullptr);
-//    struct tm *sysTime = localtime(&timer);
-//    struct tm t = *sysTime;
+    {
+        lock_guard<mutex> locker(m_mtx);
 
-//    path_ = path;
-//    suffix_ = suffix;
-//    char fileName[LOG_NAME_LEN] = {0};
-//    snprintf(fileName, LOG_NAME_LEN - 1, "%s/%04d_%02d_%02d%s",
-//             path_, t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, suffix_);
-//    toDay_ = t.tm_mday;
+        m_buff.RetrieveAll();
+        if(m_fp) {
+            flush();
+            fclose(m_fp);
+        }
 
-//    {
-//        lock_guard<mutex> locker(mtx_);
-//        buff_.RetrieveAll();
-//        if(fp_) {
-//            flush();
-//            fclose(fp_);
-//        }
+        m_fp = fopen(fileName, "a");
+        if(m_fp == nullptr) {
+            mkdir(m_path, 0777);
+            m_fp = fopen(fileName, "a");
+        }
+        assert(m_fp != nullptr);
+    }
+}
+//根据指定的日志级别和格式，将日志内容写入缓冲区
+//根据当前日期和日志行数，可能创建新的日志文件
+//如果是异步写入模式，将日志内容推入队列；否则直接将内容写入文件
+void Log::write(int level, const char *format, ...) {
+    struct timeval now = {0, 0};
+    gettimeofday(&now, nullptr);
+    time_t tSec = now.tv_sec;
+    struct tm *sysTime = localtime(&tSec);
+    struct tm t = *sysTime;
+    va_list vaList;
 
-//        fp_ = fopen(fileName, "a");
-//        if(fp_ == nullptr) {
-//            mkdir(path_, 0777);
-//            fp_ = fopen(fileName, "a");
-//        }
-//        assert(fp_ != nullptr);
-//    }
-//}
+    /* 日志日期 日志行数 */
+    if (m_toDay != t.tm_mday || (m_lineCount && (m_lineCount  %  MAX_LINES == 0)))
+    {
+        unique_lock<mutex> locker(m_mtx);
+        locker.unlock();
 
-//void Log::write(int level, const char *format, ...) {
-//    struct timeval now = {0, 0};
-//    gettimeofday(&now, nullptr);
-//    time_t tSec = now.tv_sec;
-//    struct tm *sysTime = localtime(&tSec);
-//    struct tm t = *sysTime;
-//    va_list vaList;
+        char newFile[LOG_NAME_LEN];
+        char tail[36] = {0};
+        snprintf(tail, 36, "%04d_%02d_%02d", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
 
-//    /* 日志日期 日志行数 */
-//    if (toDay_ != t.tm_mday || (lineCount_ && (lineCount_  %  MAX_LINES == 0)))
-//    {
-//        unique_lock<mutex> locker(mtx_);
-//        locker.unlock();
+        if (m_toDay != t.tm_mday)
+        {
+            snprintf(newFile, LOG_NAME_LEN - 72, "%s/%s%s", m_path, tail, m_suffix);
+            m_toDay = t.tm_mday;
+            m_lineCount = 0;
+        }
+        else {
+            snprintf(newFile, LOG_NAME_LEN - 72, "%s/%s-%d%s", m_path, tail, (m_lineCount  / MAX_LINES), m_suffix);
+        }
 
-//        char newFile[LOG_NAME_LEN];
-//        char tail[36] = {0};
-//        snprintf(tail, 36, "%04d_%02d_%02d", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
+        locker.lock();
+        flush();
+        fclose(m_fp);
+        m_fp = fopen(newFile, "a");
+        assert(m_fp != nullptr);
+    }
+    else{
+        unique_lock<mutex> locker(m_mtx);
+        m_lineCount++;
+        int n = snprintf(m_buff.BeginWrite(), 128, "%d-%02d-%02d %02d:%02d:%02d.%06ld ",
+                         t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
+                         t.tm_hour, t.tm_min, t.tm_sec, now.tv_usec);
 
-//        if (toDay_ != t.tm_mday)
-//        {
-//            snprintf(newFile, LOG_NAME_LEN - 72, "%s/%s%s", path_, tail, suffix_);
-//            toDay_ = t.tm_mday;
-//            lineCount_ = 0;
-//        }
-//        else {
-//            snprintf(newFile, LOG_NAME_LEN - 72, "%s/%s-%d%s", path_, tail, (lineCount_  / MAX_LINES), suffix_);
-//        }
+        m_buff.HasWritten(n);
+        AppendLogLevelTitle(level);
 
-//        locker.lock();
-//        flush();
-//        fclose(fp_);
-//        fp_ = fopen(newFile, "a");
-//        assert(fp_ != nullptr);
-//    }
+        va_start(vaList, format);
+        int m = vsnprintf(buff_.BeginWrite(), buff_.WritableBytes(), format, vaList);
+        va_end(vaList);
 
-//    {
-//        unique_lock<mutex> locker(mtx_);
-//        lineCount_++;
-//        int n = snprintf(buff_.BeginWrite(), 128, "%d-%02d-%02d %02d:%02d:%02d.%06ld ",
-//                         t.tm_year + 1900, t.tm_mon + 1, t.tm_mday,
-//                         t.tm_hour, t.tm_min, t.tm_sec, now.tv_usec);
+        m_buff.HasWritten(m);
+        m_buff.Append("\n\0", 2);
 
-//        buff_.HasWritten(n);
-//        AppendLogLevelTitle_(level);
+        if(isAsync_ && deque_ && !deque_->full()) {
+            deque_->push_back(m_buff.RetrieveAllToStr());
+        } else {
+            fputs(m_buff.Peek(), m_fp);
+        }
+        m_buff.RetrieveAll();
+    }
+}
 
-//        va_start(vaList, format);
-//        int m = vsnprintf(buff_.BeginWrite(), buff_.WritableBytes(), format, vaList);
-//        va_end(vaList);
+//根据日志级别在缓冲区中添加相应的日志级别标题
+void Log::AppendLogLevelTitle(int level) {
+    switch(level) {
+    case 0:
+        m_buff.Append("[debug]: ", 9);
+        break;
+    case 1:
+        m_buff.Append("[info] : ", 9);
+        break;
+    case 2:
+        m_buff.Append("[warn] : ", 9);
+        break;
+    case 3:
+        m_buff.Append("[error]: ", 9);
+        break;
+    default:
+        m_buff.Append("[info] : ", 9);
+        break;
+    }
+}
+//刷新缓冲区，并将内容写入文件。如果是异步写入模式，则刷新队列中的日志内容。
+void Log::flush() {
+    if(m_isAsync) {
+        m_deque->flush();
+    }
+    fflush(m_fp);
+}
+//异步写入模式下的写入线程函数，从队列中获取日志内容，并将其写入文件
+void Log::AsyncWrite() {
+    string str = "";
+    while(m_deque->pop(str)) {
+        lock_guard<mutex> locker(m_mtx);
+        fputs(str.c_str(), m_fp);
+    }
+}
 
-//        buff_.HasWritten(m);
-//        buff_.Append("\n\0", 2);
-
-//        if(isAsync_ && deque_ && !deque_->full()) {
-//            deque_->push_back(buff_.RetrieveAllToStr());
-//        } else {
-//            fputs(buff_.Peek(), fp_);
-//        }
-//        buff_.RetrieveAll();
-//    }
-//}
-
-//void Log::AppendLogLevelTitle_(int level) {
-//    switch(level) {
-//    case 0:
-//        buff_.Append("[debug]: ", 9);
-//        break;
-//    case 1:
-//        buff_.Append("[info] : ", 9);
-//        break;
-//    case 2:
-//        buff_.Append("[warn] : ", 9);
-//        break;
-//    case 3:
-//        buff_.Append("[error]: ", 9);
-//        break;
-//    default:
-//        buff_.Append("[info] : ", 9);
-//        break;
-//    }
-//}
-
-//void Log::flush() {
-//    if(isAsync_) {
-//        deque_->flush();
-//    }
-//    fflush(fp_);
-//}
-
-//void Log::AsyncWrite_() {
-//    string str = "";
-//    while(deque_->pop(str)) {
-//        lock_guard<mutex> locker(mtx_);
-//        fputs(str.c_str(), fp_);
-//    }
-//}
-
-//Log* Log::Instance() {
-//    static Log inst;
-//    return &inst;
-//}
-
-//void Log::FlushLogThread() {
-//    Log::Instance()->AsyncWrite_();
-//}
+Log* Log::Instance() {
+    static Log inst;
+    return &inst;
+}
+//静态函数，用于启动异步写入模式下的写入线程
+void Log::FlushLogThread() {
+    Log::Instance()->AsyncWrite();
+}
